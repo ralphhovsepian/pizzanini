c81981df2904251b9c0bfb4af1589fdb4ea27bce
Ralph Robert Hossepian <ralphhovsepian@mail.ru>
2020-06-20 21:36:25 +0400

    Updates

---
+++ precache-manifest.1b45df3a09d1e6869a6158bfcb633fe3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f99d8b12a4c1188ed8339615287a7848",
+    "url": "/pizzanini/index.html"
+  },
+  {
+    "revision": "ca8b5fd4bcc3e8ee9eff",
+    "url": "/pizzanini/static/css/main.a14040f2.chunk.css"
+  },
+  {
+    "revision": "cae27f1b2b165903a992",
+    "url": "/pizzanini/static/js/2.cd21e716.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pizzanini/static/js/2.cd21e716.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca8b5fd4bcc3e8ee9eff",
+    "url": "/pizzanini/static/js/main.b04058bd.chunk.js"
+  },
+  {
+    "revision": "ac6558077b678524db47",
+    "url": "/pizzanini/static/js/runtime-main.8488e045.js"
+  },
+  {
+    "revision": "34d9e8f47770999c84e3bfc3df5798e8",
+    "url": "/pizzanini/static/media/footerlogo.34d9e8f4.png"
+  },
+  {
+    "revision": "c2ba2319666c61c8505959d754300abe",
+    "url": "/pizzanini/static/media/hawaiian.c2ba2319.jpg"
+  },
+  {
+    "revision": "80abb4be2bc351b5e62683bba5551015",
+    "url": "/pizzanini/static/media/logo.80abb4be.png"
+  },
+  {
+    "revision": "311b477a6b79c14f5deb5b2047dc9038",
+    "url": "/pizzanini/static/media/margarita.311b477a.jpg"
+  },
+  {
+    "revision": "9043482241fd74ed2f709dfb566b0365",
+    "url": "/pizzanini/static/media/pepperoni.90434822.jpg"
+  },
+  {
+    "revision": "31229bcbbd23e7b79d63667be6899177",
+    "url": "/pizzanini/static/media/pizza.31229bcb.png"
+  },
+  {
+    "revision": "67638f2db65234f9df4384cd25b4e31b",
+    "url": "/pizzanini/static/media/veggie.67638f2d.jpg"
+  }
+]);
