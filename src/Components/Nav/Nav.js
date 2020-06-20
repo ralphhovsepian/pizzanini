307248f7208c5a7d93b870e62b0647441c713ea6
Ralph Robert Hossepian <ralphhovsepian@mail.ru>
2020-06-20 21:57:04 +0400

    Pizza nini

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -79,7 +79,9 @@ export default function Nav() {
 
       <ul className='mobileNav'>
         <li>
-          <img className='mobilelogo' src={logo} />
+          <Link to='/pizzanini/'>
+            <img className='mobilelogo' src={logo} />
+          </Link>
         </li>
 
         <li className='menuBtn' onClick={menuHandler}>
@@ -87,9 +89,11 @@ export default function Nav() {
         </li>
       </ul>
       <div className='itemsMobile'>
-        <Link to='/pizzanini/about/'>
-          <li>About</li>
-        </Link>
+        <li>
+          {' '}
+          <Link to='/pizzanini/about/'>About </Link>
+        </li>
+
         <li>
           <a href='#menu'>Menu</a>
         </li>
